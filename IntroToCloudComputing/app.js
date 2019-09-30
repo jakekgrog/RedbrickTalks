7fcb33d9406233ca9816806dc13e44461dfb699e
jakekgrogan <jake.kgrogan@gmail.com>
2019-09-30 13:34:57 +0100

    Added basic setup

--- IntroToCloudComputing/app.js
+++ IntroToCloudComputing/app.js
@@ -2,10 +2,10 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -17,10 +17,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- IntroToCloudComputing/routes/index.js
+++ IntroToCloudComputing/routes/index.js
@@ -1,11 +1,43 @@
 var express = require('express');
+var http = require('http');
 var router = express.Router();
 var AWS = require('aws-sdk')
-var DDB = new AWS.DynamoDB.DocumentClient();
+var ddb = new AWS.DynamoDB.DocumentClient();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
+  // ON EVENT => FETCH DATA from /data
   res.render('index', { title: 'Express' });
 });
+
+/* GET data from DynamoDB */
+router.get('/data', async (req, res) => {
+    const params = {
+      TableName: 'TodoList'
+    }
+    const response = await ddb.scan(params).promise();
+
+    return res.status(200).json(
+      {
+        data: response
+      }
+    );
+});
+
+/* POST to DynamoDB. */
+router.post('/add', async (req, res) => {
+    const data = req.body.task;
+    const params = {
+      TableName: 'TodoList',
+      Item: {
+        "TaskName": req.body.taskName
+      }
+    }
+    await ddb.put(params).promise();
+
+    // EMIT FETCH EVENT TO UPDATE PAGE
+    
+    return res.status(200);
+})
 
 module.exports = router;
