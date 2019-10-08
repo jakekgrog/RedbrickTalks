108fe8485df2ba618a5d333cf86577e7b8a29364
jakekgrogan <jake.kgrogan@gmail.com>
2019-10-08 14:00:35 +0100

    Updated

--- IntroToCloudComputing/client/src/app/config/api-endpoints.ts
+++ IntroToCloudComputing/client/src/app/config/api-endpoints.ts
@@ -1,5 +1,5 @@
 export const ApiEndpoints = {
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://DemoCLB-80408862.eu-west-1.elb.amazonaws.com/api',
   apiDataGet: '/get',
   apiDataPut: '/put',
   apiDataDelete: '/delete'
