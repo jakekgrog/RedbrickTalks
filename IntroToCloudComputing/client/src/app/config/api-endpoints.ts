912bd1ae36f7119f36e0d72af1b6077c5aa13f85
jakekgrogan <jake.kgrogan@gmail.com>
2019-10-09 17:56:42 +0100

    Updated docs

--- IntroToCloudComputing/client/src/app/config/api-endpoints.ts
+++ IntroToCloudComputing/client/src/app/config/api-endpoints.ts
@@ -1,5 +1,5 @@
 export const ApiEndpoints = {
-  apiUrl: 'http://DemoCLB-80408862.eu-west-1.elb.amazonaws.com/api',
+  apiUrl: 'http://talkELB-96046619.eu-west-1.elb.amazonaws.com/api',
   apiDataGet: '/get',
   apiDataPut: '/put',
   apiDataDelete: '/delete'
